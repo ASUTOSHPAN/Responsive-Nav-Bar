12a7d1556550615a0f56799c521d5961d1b93e00
Asutosh <asutoshpanda268@gmail.com>
2025-10-27 17:36:46 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let icon = document.getElementById("icon");
+icon.style.maxHeight = "0px";
+
+function toggleMenu() {
+    if (icon.style.maxHeight = "0px") {
+        icon.style.maxHeight = "0px";
+    }else{
+        icon.style.maxHeight = "opx";
+    }
+}
